ec29d415c6b89218fb9eda052d2f972a53a75319
Boyani02 <boyanibeverly20@gmail.com>
2024-08-09 08:48:25 +0300

    Second commit

--- app/src/main/java/com/example/travelada/Activity/LoginActivity.java
+++ app/src/main/java/com/example/travelada/Activity/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.example.travelada.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +11,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.travelada.R;
+import com.example.travelada.databinding.ActivityLoginBinding;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+    ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setVariable();
+
+    }
+    private void setVariable() {
+        binding.loginBtn.setOnClickListener(v -> {
+            String email = binding.userEdt.getText().toString();
+            String password = binding.passEdt.getText().toString();
+
+            if(!email.isEmpty() && !password.isEmpty()){
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(LoginActivity.this, task -> {
+                    if(task.isSuccessful()){
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }else{
+                        Toast.makeText(LoginActivity.this,"Authentication failed",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                Toast.makeText(LoginActivity.this,"Please fill username and password", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
--- app/src/main/java/com/example/travelada/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/travelada/Activity/SignUpActivity.java
@@ -1,6 +1,9 @@
 package com.example.travelada.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +12,44 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.travelada.R;
+import com.example.travelada.databinding.ActivitySignUpBinding;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends BaseActivity {
+
+    ActivitySignUpBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_up);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setVariable();
+    }
+
+    private void setVariable() {
+        binding.signupBtn.setOnClickListener(v -> {
+            String name = binding.nameEdt.getText().toString();
+            String email = binding.userEdt.getText().toString();
+            String username = binding.usernameEdt.getText().toString();
+            String password = binding.passEdt.getText().toString();
+
+            if(password.length()<6){
+                Toast.makeText(SignUpActivity.this,"Password must be 6 characters",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(SignUpActivity.this, task -> {
+                if(task.isSuccessful()){
+                    Log.i(TAG,"onComplete: ");
+                    startActivity(new Intent(SignUpActivity.this,MainActivity.class));
+                }else{
+                    Log.i(TAG, "failure: " +task.getException());
+                    Toast.makeText(SignUpActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
         });
     }
 }
